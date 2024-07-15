82f66eac8f18563522b4c8cc0c770ad53023fcfa
Kolistratova Julia <kolistratova.julija@mail.ru>
2024-07-16 04:46:47 +0600

    Добавьте файлы проекта.

---
+++ Lab0_WorkingWithVariablesAndBranches/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Lab0_WorkingWithVariablesAndBranches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            int year = ReadYear();
+            bool isLeapYear = IsLeapYear(year);
+            WriteYear(year, isLeapYear);
+
+            Console.WriteLine("\nЗадание 2");
+            int temperature = 0;
+            string scale = "";
+            ReadTemperature(ref temperature, ref scale);
+            double convertTemp = ConvertTemperature(temperature, ref scale);
+            WriteTemperature(convertTemp, scale);
+
+            Console.ReadKey();
+        }
+
+        // Задание 1
+
+        static int ReadYear()
+        {
+            Console.WriteLine("Введите год: ");
+            string input = Console.ReadLine();
+            int year = int.Parse(input);
+            return year;
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            var isLeapYear = false;
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                isLeapYear = true;
+            }
+            else if (year % 400 == 0)
+            {
+                isLeapYear = true;
+            }
+            return isLeapYear;
+        }
+
+        static void WriteYear(int year, bool isLeapYear)
+        {
+            Console.WriteLine($"{year} - {(isLeapYear ? "високосный" : "невисокосный")} год");
+        }
+
+        // Задание 2
+
+        static void ReadTemperature(ref int temperature, ref string scale)
+        {
+            Console.WriteLine("Введите значение температуры: ");
+            temperature = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите значение шкалы: ");
+            scale = Console.ReadLine();
+        }
+
+        static double ConvertTemperature(int temperature, ref string scale)
+        {
+            var convertedTemp = 0.0;
+            if (scale.ToUpper() == "C")
+            {
+                scale = "F";
+                double temp = (temperature * 9) / 5 + 32;
+                convertedTemp = Math.Round(temp);
+            }else if (scale.ToUpper() == "F")
+            {
+                scale = "C";
+                double temp = (temperature - 32) * 5 / 9;
+                convertedTemp = Math.Round(temp);
+            }
+            return convertedTemp;
+        }
+
+        static void WriteTemperature(double temperature, string scale)
+        {
+            Console.WriteLine("Результат:" + temperature.ToString() + scale.ToString());
+        }
+    }
+}
---
+++ Lab1_WorkingWithLoops/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab1_WorkingWithLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Вводите слова, завершая каждое нажатием Enter.");
+            Console.WriteLine("Для выхода наберите \"exit\".\n");
+            List<string> words = ReadWords();
+            string longestWord = FindLongestWord(words, out int maxLenghtWord);
+            Console.WriteLine("\nСчитывание завершено.");
+            Console.WriteLine($"Самое длинное слово: \"{longestWord.ToUpper()}\" ({maxLenghtWord})");
+            
+            Console.WriteLine("\nЗадание 2");
+            PlayHeadsOrTails();
+
+            Console.ReadKey();
+        }
+
+
+        // Задание 1
+        static List<string> ReadWords()
+        {
+            List<string> words = new List<string>();
+            var word = "";
+            while ((word = Console.ReadLine()) != "exit")
+            {    
+                words.Add(word);
+            }
+            return words;
+        }
+
+        static string FindLongestWord(List<string> words, out int maxLenghtWord)
+        {
+            maxLenghtWord = 0;
+            string longestWord = "";
+            foreach (string word in words)
+            {
+                if (word.Length > maxLenghtWord)
+                {
+                    maxLenghtWord = word.Length;
+                    longestWord = word;
+                }
+            }
+            return longestWord;
+        }
+
+        // Задание 2
+        static void PlayHeadsOrTails()
+        {
+            Console.WriteLine("Игра началась!");
+            Random random = new Random();
+            var numberOfAttempts = 0;
+            var numberOfVictories = 0;
+            int number = 0;
+            while (true) 
+            {
+                Console.WriteLine("Введите число: ");
+                var input = Console.ReadLine();
+                number = int.Parse(input);
+
+                if(number != 0 && number != 1)
+                {
+                    break;
+                }
+                else if (number == random.Next(2))
+                {
+                    Console.WriteLine("Угадали!");
+                    numberOfVictories++;
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте снова");
+                }
+                numberOfAttempts++;
+            } 
+            Console.WriteLine($"Игра окончена со счётом {numberOfVictories}, " +
+            $"угадано {CalculateWinningPercentage(numberOfVictories, numberOfAttempts)}% бросков.");
+        }
+
+        static double CalculateWinningPercentage(int numberOfVictories, int numberOfAttempts)
+        {
+            var result = 0.0;
+            if (numberOfVictories != 0)
+            {
+                var temp = (double)numberOfVictories / numberOfAttempts * 100;
+                result = Math.Round(temp);
+            }
+            return result;
+        }
+    }
+}
